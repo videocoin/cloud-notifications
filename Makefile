9c59abb55de0cc92a7a3806add79fed3ca70bdc6
Aleksandr Didenko <aa.didenko@yandex.com>
2020-06-11 22:50:35 +0300

    Prepare docker build process for new infra

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 GOOS?=linux
 GOARCH?=amd64
-
-GCP_PROJECT=videocoin-network
+ENV?=dev
 
 NAME=notifications
 VERSION=$$(git describe --abbrev=0)-$$(git rev-parse --abbrev-ref HEAD)-$$(git rev-parse --short HEAD)
-ENV?=dev
+
+REGISTRY_SERVER?=registry.videocoin.net
+REGISTRY_PROJECT?=cloud
 
 .PHONY: deploy
 
@@ -26,16 +27,13 @@ deps:
 
 lint: docker-lint
 
+release: docker-build docker-push
+
 docker-lint:
 	docker build -f Dockerfile.lint .
 
 docker-build:
-	docker build -t gcr.io/${GCP_PROJECT}/${NAME}:${VERSION} -f Dockerfile .
+	docker build -t ${REGISTRY_SERVER}/${REGISTRY_PROJECT}/${NAME}:${VERSION} -f Dockerfile .
 
 docker-push:
-	docker push gcr.io/${GCP_PROJECT}/${NAME}:${VERSION}
-
-release: docker-build docker-push
-
-deploy:
-	ENV=${ENV} GCP_PROJECT=${GCP_PROJECT} deploy/deploy.sh
+	docker push ${REGISTRY_SERVER}/${REGISTRY_PROJECT}/${NAME}:${VERSION}
--- service/config.go
+++ service/config.go
@@ -5,16 +5,14 @@ import (
 )
 
 type Config struct {
-	Name    string `envconfig:"-"`
-	Version string `envconfig:"-"`
+	Name    string        `envconfig:"-"`
+	Version string        `envconfig:"-"`
+	Logger  *logrus.Entry `envconfig:"-"`
 
-	MQURI     string `default:"amqp://guest:guest@127.0.0.1:5672" envconfig:"MQURI"`
-	Templates string `default:"templates" envconfig:"TEMPLATES"`
-	Env       string `default:"dev" envconfig:"ENV"`
-	RPCAddr   string `required:"true" envconfig:"RPC_ADDR" default:"127.0.0.1:5005"`
-	FromEmail string `default:"support@videocoin.network"`
-
-	InternalEmails string `default:"dmitry@liveplanet.net adidenko@liveplanet.net" envconfig:"INTERNAL_EMAILS"`
-
-	Logger *logrus.Entry `envconfig:"-"`
+	MQURI          string `envconfig:"MQURI" default:"amqp://guest:guest@127.0.0.1:5672"`
+	Templates      string `envconfig:"TEMPLATES" default:"templates"`
+	Env            string `envconfig:"ENV" default:"dev"`
+	RPCAddr        string `envconfig:"RPC_ADDR" default:"127.0.0.1:5005"`
+	FromEmail      string `default:"support@videocoin.network"`
+	InternalEmails string `envconfig:"INTERNAL_EMAILS" default:"dmitry@liveplanet.net adidenko@liveplanet.net"`
 }
